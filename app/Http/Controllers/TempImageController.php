8ca536b6231b38cbbb13f30f00bad92114243627
Nischal <nischalshrestha800@gmail.com>
2024-06-21 07:58:52 +0545

    Blog App Backend

---
+++ app/Http/Controllers/TempImageController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TempImage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TempImageController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Please Fix Errors',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        // upload image here
+        $image = $request->image;
+        $ext = $image->getClientOriginalExtension();
+        $imageName = time() . '.' . $ext;
+
+        // /store  Image info in database
+        $tempImage = new TempImage();
+        $tempImage->name = $imageName;
+        $tempImage->save();
+
+
+        // move image in temp dir
+        $image->move(public_path('uploads/temp'), $imageName);
+        return response()->json([
+            'status' => true,
+            'message' => 'Image uploaded successfully.',
+            'errors' => $tempImage
+        ]);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\TempImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('blogs', [BlogController::class, 'store']);
+Route::post('save-temp-image', [TempImageController::class, 'store']);
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
