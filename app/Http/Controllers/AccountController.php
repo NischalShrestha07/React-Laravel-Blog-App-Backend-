1c55fe04327cfbbb2da8ca81a6d0dbdaa5025de5
Nischal <nischalshrestha800@gmail.com>
2024-06-23 03:01:48 +0545

    Blog App Backend

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    //method will show register page
+    public function register()
+    {
+        return view('account.register');
+
+    }
+}
